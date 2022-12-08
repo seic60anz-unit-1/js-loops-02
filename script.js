cf660684dab48c62787dd9ce023d0fd46349b2b4
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-08 12:14:27 +1100

    It is a difficult guess

--- script.js
+++ script.js
@@ -8,15 +8,40 @@
 
 //Every 2
 
-let i = 0
-while (i <= 8) {
-    if (i === 0) {
-        console.log(i)
-        i++
-        continue
+// let i = 0
+// while (i <= 8) {
+//     if (i === 0) {
+//         console.log(i)
+//         i++
+//         continue
+//     }
+//     if (i % 2 === 0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+//Guess The Number
+
+let x = parseInt(Math.random()*10)
+
+let input = 11
+while (input != x) {
+    input = parseInt(prompt("Guess the number between 0 and 10"))
+    while (!Number.isInteger(input) || input > 10 || input < 0) {
+            alert("Please enter an integer between 0 and 10!")
+            input = parseInt(prompt("Guess the number between 0 and 10"))
     }
-    if (i % 2 === 0){
-        console.log(i)
+    alert(`You guess it is ${input}!`)
+
+    if (input === x) {
+        alert("Congrat! You guess it correctly!")
     }
-    i++
+    if (input > x) {
+        alert("Wrong, guess lower!")
+    }
+    if (input < x) {
+        alert("Wrong, guess higher!")
+    }
+    
 }
