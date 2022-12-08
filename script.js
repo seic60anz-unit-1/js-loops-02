8e94f4526b85b0ac07d14db390f68eee25adfc1b
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-08 11:08:28 +1100

    Every 2

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 //Count to 9
 
+// let i = 0
+// while (i <= 9) {
+//     console.log (i)
+//     i++
+// }
+
+//Every 2
+
 let i = 0
-while (i <= 9) {
-    console.log (i)
+while (i <= 8) {
+    if (i === 0) {
+        console.log(i)
+        i++
+        continue
+    }
+    if (i % 2 === 0){
+        console.log(i)
+    }
     i++
 }
