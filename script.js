88dda03d5931cebec6ff40e2be58eef164ffb0da
Penelope Chen <pen.c828@gmail.com>
2022-12-08 12:02:40 +1100

    forgot the loop but it just makes it complicated

--- script.js
+++ script.js
@@ -15,10 +15,15 @@
 // Guess The Number 
 const num = 9
 let answer = parseInt(prompt("Guess a number between 0 & 10."))
-if (answer === 9) {
-    console.log("Congrats! You guessed it right!")
-} else {
- console.log("Guess again")
+
+for (let i = 1; i < 10; i++) {
+    if (i % 9 !== 0) {
+        console.log("Guess again")
+        break
+    } else if (i % 9 === 0) {
+        console.log("Congrats! You guessed it right!")
+        break
+    } 
 }
 
 // parseInt("", 8)
