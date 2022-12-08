b12ec2cf12412401ba5d89b53bef9e53f994bb68
waddo4 <109324137+waddo4@users.noreply.github.com>
2022-12-08 12:46:34 +1100

    Loops answers

---
+++ script.js
@@ -0,0 +1,33 @@
+//count to 9
+
+// i=0
+// while (i<10){
+//     console.log(i);
+//     i++
+// }
+
+//every 2
+
+// i=0
+// while (i<10) {
+//     if (i%2 === 0) {
+//     console.log(i);
+//     }   
+//     i++ 
+// }
+
+//guess the number
+
+// const number = Math.floor(Math.random() * 11)
+
+// while (true) {
+//     let userGuess = parseInt(prompt ("What is your guess?"));
+//     if (userGuess === number){
+//         console.log("Congrats");
+//         break
+//     } else if (userGuess > number){
+//         console.log("too high");
+//     } else if (userGuess < number){
+//         console.log("too low");
+// }
+// }
